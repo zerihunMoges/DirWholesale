8a17496ac05c99746242af7155b2bfcede2f2487
hayat <hayattofik22@gmail.com>
2023-01-20 18:06:35 +0300

    added CRUD for payment_detail

--- DirBackend/src/resource/payement_detail/payement_detail.controller.ts
+++ DirBackend/src/resource/payement_detail/payement_detail.controller.ts
@@ -1,7 +1,8 @@
 import { NextFunction } from 'express'
 import { Payment_detail } from './payement_detail.model'
+import {Request ,Response} from 'express'
 
-export async function addpayment_detail(req, res, next: NextFunction) {
+export async function addpayment_detail(req:Request, res:Response, next: NextFunction) {
   try {
     const {  Order_Id,
         amount,
@@ -21,8 +22,9 @@ export async function addpayment_detail(req, res, next: NextFunction) {
 
 export async function getPayment_detail(req, res, next: NextFunction) {
   try {
-    const { _id } = req.params
-    const payment_detail = await Payment_detail.findById(_id)
+    const { id } = req.params
+    console.log(id)
+    const payment_detail = await Payment_detail.findById(id)
     if (payment_detail) {
       return res.status(200).json(payment_detail)
     }
@@ -41,3 +43,47 @@ export async function getPayment_details(req, res, next: NextFunction) {
     res.status(500).json({ message: err.message })
   }
 }
+
+export async function updatePayement_detail(req, res, next)  {
+    try {
+        const { id } = req.params
+        console.log(id)
+      const payment = await Payment_detail.findByIdAndUpdate(id, req.body, {
+        new: true,
+       
+      });
+      if (!payment) {
+        res.status(404).json({
+          status: "error",
+          message: "payment Detail with this ID does not exist",
+        });
+      }
+      res.status(200).json({
+        status: "success",
+        payment,
+      });
+    } catch (err) {
+ 
+      console.log("can't Update the payment")
+    }
+  };
+  export async function deletePayment_detail (req, res, next) {
+    try {
+        const { id } = req.params
+        console.log(id)
+      const payment = await Payment_detail.findByIdAndDelete(id);
+      if (!payment) {
+        res.status(404).json({
+          status: "error",
+          message: "Payment with this ID does not exist",
+        });
+      }
+      res.status(204).json({
+        status: "success",
+       payment: null,
+      });
+    } catch (err) {
+      //TODO
+      console.log("error in deleting payment detail")
+    }
+  };
